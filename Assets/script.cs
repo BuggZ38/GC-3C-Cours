a3b596315485c28daa697ee2c9669ca4224f694e
STAUSS Baptiste <baptiste.st38@gmail.com>
2026-09-22 11:45:59 +0200

    push

--- Assets/script.cs
+++ Assets/script.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class script : MonoBehaviour
 {
 
-    [Header("Movement")]
+    [Header("Movement Settings")]
     [SerializeField] float _speed;
     [SerializeField] Vector3 _direction;
+    [SerializeField] float _jumpForce;
+    [SerializeField] float _jumpRaycastDistance;
+
+    [Header ("References")]
     [SerializeField] InputActionReference _moveInput;
+    [SerializeField] InputActionReference _jumpInput;
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] GameObject _footPosition;
+
+    bool _isGrounded;
 
 
     void Reset()
     {
+        
         _speed = 5f;
         _direction = Vector3.forward;
+        _jumpForce = 5f;
+        _jumpRaycastDistance = 1f;
+    }
+
+    void Awake()
+    {
+        _moveInput.action.Enable();
+        _jumpInput.action.Enable();
     }
 
 
@@ -26,11 +45,24 @@ public class script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _isGrounded = Physics.Raycast(_footPosition.transform.position, Vector3.down, _jumpRaycastDistance);
 
         Vector2 direction = _moveInput.action.ReadValue<Vector2>();
 
         Vector3 moveDirection = new Vector3(direction.x, 0, direction.y);
 
         transform.Translate(moveDirection * (_speed * Time.deltaTime));
+
+        Debug.DrawRay(_footPosition.transform.position, Vector3.down * _jumpRaycastDistance, Color.red);
+
+        if (_jumpInput.action.WasPressedThisFrame() && _isGrounded)
+        {
+            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
     }
 }
